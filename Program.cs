69288ca69c70c4a30d554e50fe75f3b83906d34e
iskall94 <isakerik@gmail.com>
2025-10-12 00:13:49 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AppWithAISupport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
